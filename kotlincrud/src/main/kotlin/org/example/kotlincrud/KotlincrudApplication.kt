0ddb7fa3e82477f57dbdc53456b1fb9ab6af4ab4
하이 <kkm07133@naver.com>
2025-01-07 16:30:11 +0900

    feat :: 프로젝트 빌드

---
+++ kotlincrud/src/main/kotlin/org/example/kotlincrud/KotlincrudApplication.kt
@@ -0,0 +1,11 @@
+package org.example.kotlincrud
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlincrudApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlincrudApplication>(*args)
+}
