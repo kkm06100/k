cc83b79ac7095abc4b5fc590077f6dcb634f5117
김경민 <kkm07133@naver.com>
2025-01-07 21:00:00 +0900

    Revert "Feature/post pull Request"

--- kotlincrud/src/main/kotlin/org/example/kotlincrud/post/persistence/PostRepository.kt
+++
@@ -1,8 +0,0 @@
-package org.example.kotlincrud.post.persistence
-
-import org.example.kotlincrud.post.persistence.domain.Post
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface PostRepository : JpaRepository<Post, Long> {
-
-}
